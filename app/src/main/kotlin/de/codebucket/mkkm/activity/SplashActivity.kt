146efe57ffcd9df1d65ff15b539fd2868e6bf8e9
David Sn <divad.nnamtdeis@gmail.com>
2021-02-03 03:45:32 +0100

    Add splash screen and finish brand theming
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- app/src/main/kotlin/de/codebucket/mkkm/activity/SplashActivity.kt
+++ app/src/main/kotlin/de/codebucket/mkkm/activity/SplashActivity.kt
@@ -1,6 +1,9 @@
 package de.codebucket.mkkm.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +18,18 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, WebViewActivity::class.java)
+            launch(intent);
+        }, 1500L)
     }
 
+    private fun launch(intent: Intent) {
+        runOnUiThread {
+            startActivity(intent)
+            overridePendingTransition(0, android.R.anim.fade_out)
+            finish()
+        }
+    }
 }
