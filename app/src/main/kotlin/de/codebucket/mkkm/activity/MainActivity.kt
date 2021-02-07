a72f3536a233d5b87d0b347614ef52a895a84fb8
David Sn <divad.nnamtdeis@gmail.com>
2021-02-07 05:34:02 +0100

    Revert "Clear cache on launch before loading app"
    This reverts commit 3a948154f488980ed50a7d69378ca086949aa5c0.

--- app/src/main/kotlin/de/codebucket/mkkm/activity/MainActivity.kt
+++ app/src/main/kotlin/de/codebucket/mkkm/activity/MainActivity.kt
@@ -88,7 +88,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Load the app
-        webview.clearCache(true)
         webview.loadUrl("https://m.kkm.krakow.pl")
 
         val preferences = MobileKKM.preferences
